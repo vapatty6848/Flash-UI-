4343c30f1050475370ef741bc0723174dfbb7542
vapatty6848 <vapatty@gmail.com>
2023-12-04 18:38:39 -0300

    feat: Homepage, package Tokens

---
+++ apps/web/src/app/component/progress-circle.tsx
@@ -0,0 +1,29 @@
+export interface ProgressCircleProps {
+  progress: number;
+}
+
+export function ProgressCircle(props: ProgressCircleProps) {
+  return (
+    <svg className="w-full h-full" viewBox="0 0 100 100">
+      <circle
+        className="text-smoke-400 stroke-current"
+        strokeWidth="12"
+        cx="50"
+        cy="50"
+        r="40"
+        fill="transparent"
+      ></circle>
+
+      <circle
+        className="text-mirage-500 progress-ring__circle stroke-current"
+        strokeWidth="12"
+        strokeLinecap="round"
+        cx="50"
+        cy="50"
+        r="40"
+        fill="transparent"
+        strokeDashoffset="calc(400 - (400 * 45) / 100)"
+      ></circle>
+    </svg>
+  )
+}
